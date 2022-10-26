ad425349457caf711f4307bedae788b91a806b2a
faria-r <fariacse11@gmail.com>
2022-10-26 19:26:43 +0600

    carousal added in home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,18 +5,26 @@ const Home = () => {
   return (
   <div className="min-h-screen">
       <div className="lg:h-full sm:h-64 xl:h-96 2xl:h-full  mt-8 w-3/4 mx-auto">
-      <Carousel slideInterval={2000}>
+      <Carousel slideInterval={3000}>
         <img
-          src="https://th.bing.com/th/id/OIP.QKutpElR_69MkWGFuOQdowHaEV?pid=ImgDet&w=520&h=304&rs=1"
+          src="https://i.ibb.co/4d39pyC/mongoDB.jpg"
+          alt="..."
+        />
+        <img
+          src="https://i.ibb.co/xFHMCdy/vue.jpg"
+          alt="..."
+        />
+        <img
+          src="https://i.ibb.co/TM0D4CK/React.jpg"
           alt="..."
         />
 
         <img
-          src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+          src="https://i.ibb.co/zf1nRqv/bootstrap.jpg"
           alt="..."
         />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+        <img className=""
+          src="https://i.ibb.co/Ycqm3pQ/OIP.jpg"
           alt="..."
         />
       </Carousel>
